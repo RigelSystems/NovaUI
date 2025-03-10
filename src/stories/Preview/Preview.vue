14844d36a1c3d6ecd652416d78fe3b2ccfa1a41b
Alex Ford <alex96ford19@gmail.com>
2025-03-10 20:25:05 +0000

    Add hero section to the preview

--- src/stories/Preview/Preview.vue
+++ src/stories/Preview/Preview.vue
@@ -4,6 +4,7 @@
   import NNavigationBar from '../NNavigationBar/NNavigationBar.vue';
   import NCard from '../NCard/NCard.vue';
   import NRow from '../NRow/NRow.vue';
+  import NHeroSection from '../NHeroSection/NHeroSection.vue';
   import { NovaUIConfigSymbol } from '../../../index';
 
   interface Link {
@@ -17,6 +18,7 @@
       NNavigationBar,
       NCard,
       NRow,
+      NHeroSection,
     },
     props: {
       label: {
@@ -43,6 +45,9 @@
 <template>
   <div class="preview">
     <NNavigationBar :links="links"/>
+
+    <NHeroSection title="Hero Section Title" subtitle="Hero Section Subtitle" content="Some example text for this hero section" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+
     <NRow :cols="{sm: [100], md: [60,40], lg: [33.33,33.33,33.33]}">
       <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
       <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />

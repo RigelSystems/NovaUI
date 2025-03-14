3c28e4de8fad500839adc390f83593a30bd93cb8
Alex Ford <alex96ford19@gmail.com>
2025-03-14 15:14:41 +0000

    Update preview to reflect changes

--- src/stories/Preview/Preview.vue
+++ src/stories/Preview/Preview.vue
@@ -53,22 +53,28 @@
   <div class="preview">
     <NNavigationBar :links="links"/>
 
-    <NHeroSection title="Hero Section Title" subtitle="Hero Section Subtitle" content="Some example text for this hero section" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NHeroSection title="Hero Section Title" subtitle="Hero Section Subtitle" content="Some example text for this hero section" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
 
-    <NRow :cols="{sm: [100], md: [60,40,40,60], lg: [33.33,33.33,33.33]}">
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
-      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', url: '/home'}]" />
+    <NRow :cols="{sm: [100], md: [60,40,40,60], lg: [33.33,33.33,33.33]}" title="Row Title" subtitle="Row Subtitle" :style="{background: '#f5f5f5', padding: '1rem'}">
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home', primary: true}, {label: 'Action Button', href: '/home', primary: false}]" />
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
+      <NCard title="Card Title" subtitle="Card Subtitle" content="Some example text for this card" :actionButtons="[{label: 'Action Button', href: '/home'}]" />
     </NRow>
+
+    <hr>
+
+    <h3>Dropdowns</h3>
 
     <NRow :cols="{sm: [100], md: [33.3,33.3,33.3]}">
       <NDropdown label="Click Me" content="Some hidden information!" />
       <NDropdown label="Don't click me!" content="What did I say..." />
       <NDropdown label="Ummmmm" content="Wow, you're an explorer!" />
     </NRow>
+
+    <hr>
 
     <h2>Contact Form</h2>
     <p>Please enter your details below and we'll contact you shortly!</p>
@@ -80,6 +86,5 @@
       <NTextInput label="Message" placeholder="Enter your message here" />
       <NButton label="Submit" />
     </NRow>
-
   </div>
 </template>

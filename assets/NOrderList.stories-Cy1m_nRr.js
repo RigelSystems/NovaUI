cedd407027b297e6295e4510f952a602c621d6e2
Fordalex <Fordalex@users.noreply.github.com>
2025-03-15 12:37:40 +0000

    deploy: 5e147d24c3466491e19907b307237a6506cfcf3f

--- assets/NOrderList.stories-Cy1m_nRr.js
+++ assets/NOrderList.stories-Cy1m_nRr.js
@@ -1,4 +1,4 @@
-import{N as s}from"./NOrderList-j0JR1Ubo.js";import"./vue.esm-bundler-eZClCnrB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const i={title:"Components/NOrderList",component:s,argTypes:{updateUrl:{control:"text"}}},o=a=>({components:{NOrderList:s},setup(){return{args:a}},template:`
+import{N as s}from"./NOrderList-D1bHzDix.js";import"./vue.esm-bundler-DvdU1QRB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const i={title:"Components/NOrderList",component:s,argTypes:{updateUrl:{control:"text"}}},o=a=>({components:{NOrderList:s},setup(){return{args:a}},template:`
     <NOrderList v-bind="args">
       <template #default="item">
         <p>{{ item.name }}</p>

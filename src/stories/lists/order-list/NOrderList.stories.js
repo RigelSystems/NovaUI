6e13413a2375709c7022025adc63e2b2321fd3f4
Alex Ford <alex96ford19@gmail.com>
2025-03-15 13:05:22 +0000

    Update story style for NOrderList

--- src/stories/lists/order-list/NOrderList.stories.js
+++ src/stories/lists/order-list/NOrderList.stories.js
@@ -1,33 +1,44 @@
 import NOrderList from './NOrderList.vue';
 
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: 'Components/NOrderList',
   component: NOrderList,
+  tags: ['autodocs'],
   argTypes: {
-    updateUrl: { control: 'text' },
+    updateUrl: { control: { type: 'text' } },
+    items: { control: { type: 'object' } },
   },
+  args: {
+    updateUrl: 'http://localhost:3000/checklists/update_order',
+    items: [
+      { id: 1, name: 'Item 1', order: 1 },
+      { id: 2, name: 'Item 2', order: 2 },
+      { id: 3, name: 'Item 3', order: 3 },
+    ],
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: "An ordered list component that allows reordering via drag-and-drop."
+      }
+    }
+  }
 };
 
-const Template = (args) => ({
-  components: { NOrderList },
-  setup() {
-    return { args };
-  },
-  template: `
-    <NOrderList v-bind="args">
-      <template #default="item">
-        <p>{{ item.name }}</p>
-      </template>
-    </NOrderList>
-  `,
-});
-
-export const Default = Template.bind({});
-Default.args = {
-  items: [
-    { id: 1, name: 'Item 1', order: 1 },
-    { id: 2, name: 'Item 2', order: 2 },
-    { id: 3, name: 'Item 3', order: 3 },
-  ],
-  updateUrl: 'http://localhost:3000/checklists/update_order',
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default = {
+  render: (args) => ({
+    components: { NOrderList },
+    setup() {
+      return { args };
+    },
+    template: `
+      <NOrderList v-bind="args">
+        <template #default="item">
+          <p>{{ item.name }}</p>
+        </template>
+      </NOrderList>
+    `,
+  }),
 };
